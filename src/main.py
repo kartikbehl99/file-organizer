2e298e35f14f0d8ae69cff4d0e872f10fc564673
Kartik Behl <kartikbehl99@gmail.com>
2020-11-07 23:27:28 +0530

    Fix space in directory name

--- src/main.py
+++ src/main.py
@@ -20,9 +20,10 @@ def escape_chars(filename: str) -> str:
     for i in range(len(split_filename)):
         split_filename[i] = "\\'".join(split_filename[i].split("'"))
         split_filename[i] = '\\"'.join(split_filename[i].split('"'))
+        split_filename[i] = "\\(".join(split_filename[i].split('('))
+        split_filename[i] = "\\)".join(split_filename[i].split(')'))
     
     return "\\ ".join(split_filename)
-
 
 def get_distinct_file_types(directory_path: str) -> Set[str]:
     """Gets distinct file types.
@@ -42,7 +43,7 @@ def get_distinct_file_types(directory_path: str) -> Set[str]:
         content_abs_path = directory_path + "/" + content
 
         if os.path.isfile(content_abs_path):
-            content_info: str = os.popen(f"file --mime-type {directory_path + '/' + escaped_content_name}").read()
+            content_info: str = os.popen(f"file --mime-type {escape_chars(directory_path) + '/' + escaped_content_name}").read()
             content_mimetype: str = "_".join(content_info.split()[-1].split("/"))
             file_types.add(content_mimetype)
             file_types_mapping[content] = directory_path + "/" + content_mimetype
@@ -104,10 +105,15 @@ def cleanup(directory_path: str, file_types: Set[str]):
 
 
 if __name__ == "__main__":
-    directory_path: str = input("Enter absolute path of directory you want to organize: ").strip()
 
-    if not os.path.isabs(directory_path):
-        print("Please enter absolute path")
+    while True:
+        directory_path: str = input("Enter absolute path of directory you want to organize: ").strip()
+
+        if not os.path.isabs(directory_path):
+            print("Please enter absolute path")
+        
+        else:
+            break
     
     file_types: Set[str] = get_distinct_file_types(directory_path=directory_path)
     create_directories(directory_path=directory_path, file_types=file_types)
